bee956d4acaa3e93e826dd9fa1345961217d1107
alacan-2024010 <alacan-2024010@kinal.edu.gt>
2026-04-16 14:29:47 -0600

    Se modificaron algunas cosas

--- client-admin/src/features/auth/store/authStore.js
+++ client-admin/src/features/auth/store/authStore.js
@@ -1,43 +1,38 @@
-import {create} from "zustand";
-import { persist} from "zustand/middleware";
-import {login as loginRequest} from "../../../shared/api";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { login as loginRequest } from "../../../shared/api"
 
 export const useAuthStore = create(
     persist(
-        (set,get) => ({
+        (set, get) => ({
             user: null,
             token: null,
-            expiresAt: null,
+            expiresAt:  null,
             loading: false,
             error: null,
             isAuthenticated: false,
-        
-            login: async (data) => {
-                try {
-                    set({ loading: true, error: null });
 
-                    const { data } = await loginRequest({emailOrUsername, password});
-                    console.log(data);
-                    set({ 
+            login: async ({emailOrUsername, password}) => {
+                try {
+                    set({ loading: true, error: null});
+                    const { data } = await loginRequest({emailOrUsername, password})
+                    console.log(data)
+
+                    set({
                         user: data.userDetails,
                         token: data.token,
                         expiresAt: data.expiresAt,
                         loading: false,
-                    });
+                    })
 
-                    return { success: true };
+                    return { success: true}
 
-                } catch (error) {
-                    console.error("Error en el login:", error);
-
-                    const message = error.response?.data?.message || "Error desconocido";
-
-                    set({ error: message, loading: false });
-
-                    return {
-                        success: false,
-                        error: message
-                    }
+                } catch (err) {
+                    console.error("Login error:", err);
+                    const message =
+                        err.response?.data?.message || "Error de autenticación";
+                    set({ error: message, loading: false})
+                    return { success: false, error: message}
                 }
             }
         })
--- client-admin/src/shared/api/auth.js
+++ client-admin/src/shared/api/auth.js
@@ -1,8 +1,5 @@
-import {axiosAuth} from "./api";
+import { axiosAuth } from "./api";
 
-export const login = async (data)=> { 
-    return await axiosAuth.post(
-        "/auth/login",
-        data
-    )
+export const login = async (data) => {
+    return await axiosAuth.post("/auth/login", data)
 }
