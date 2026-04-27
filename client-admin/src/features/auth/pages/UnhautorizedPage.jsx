6a3ddfab8e6082368bbf4485319a8725e4baec9f
alacan-2024010 <alacan-2024010@kinal.edu.gt>
2026-04-27 11:45:42 -0600

    Se agregaron cambios

---
+++ client-admin/src/features/auth/pages/UnhautorizedPage.jsx
@@ -0,0 +1,8 @@
+export  const UnhautorizedPage = () => {
+    return (
+        <div style={{textAlign: "center", marginTop: "50px"}}>
+            <h2>Acceso Denegado</h2>
+            <p>Solo los administradores pueden acceder a esta sección.</p>
+        </div>
+    )
+}
